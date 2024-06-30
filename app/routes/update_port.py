546ba662368de622ff7564adff824a71f7792340
zemetsskiy <zemetsskiy@gitlab.com>
2024-06-30 17:00:41 +0000

    upd

--- app/routes/update_port.py
+++ app/routes/update_port.py
@@ -6,8 +6,12 @@ update_port_bp = Blueprint('update_port', __name__)
 @update_port_bp.route('/', methods=['POST'], strict_slashes=False)
 def update_port():
     data = request.json
-    ip = data['ip']
-    old_port = data['port']
-    username = data['username']
-    new_port = update_squid_port(ip, old_port, username)
-    return jsonify({'status': 'success', 'new_port': new_port})
+    ip_ports = data['ip_ports']
+    
+    updated_ports = []
+    for ip_port in ip_ports:
+        ip, old_port, username = ip_port.split(':')
+        new_port = update_squid_port(ip, old_port, username)
+        updated_ports.append(f"{ip}:{new_port}")
+    
+    return jsonify({'status': 'success', 'updated_ports': updated_ports}) 
